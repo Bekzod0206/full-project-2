b2caa3e6ed7f92e48bf0d380c2e70dbbbe45d60e
Bekzod0206 <davronovbekzod20@gmail.com>
2023-04-21 23:51:29 +0500

    create article ui

--- src/views/CreateArticleView.vue
+++ src/views/CreateArticleView.vue
@@ -1,9 +1,36 @@
 <template>
-  <h1>Create article</h1>
+  <p class="text-center display-2">Create article</p>
+  <div class="w-50 mx-auto">
+    <form @submit.prevent>
+      <Input type="text" label="Title" v-model="title"/>
+      <TextArea type="text" label="Description" v-model="description"/>
+      <TextArea type="text" label="Body" v-model="body"/>
+      <Button @click="createArticleHandler">Create article</Button>
+    </form>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      title: '',
+      description: '',
+      body: '',
+    }
+  },
+  methods: {
+    createArticleHandler() {
+      const article = {
+        title: this.title,
+        description: this.description,
+        body: this.body,
+        tagList: []
+      }
+
+    }
+  }
+}
 </script>
 
 <style> </style>
